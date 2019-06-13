8b2b5d610e9bd3723d54a7a06195884849bd935c
Patrick Cornnell <pcornnell@gmail.com>
2019-06-13 12:49:50 -0400

    Sending orientation and acceleration

--- client/sense.py
+++ client/sense.py
@@ -1,31 +1,33 @@
 import urllib3
+import json
 from sense_hat import SenseHat
-from urllib3.exceptions import NewConnectionError
 
 
 def run_it():
-    send_things(sense_things())
+    while True:
+        send_things(sense_things())
 
 
 def sense_things():
-    # while True:
-        sense = SenseHat()
-        o = sense.get_orientation()
-        pitch = o["pitch"]
-        roll = o["roll"]
-        yaw = o["yaw"]
-        print("pitch {0} roll {1} yaw {2}".format(pitch, roll, yaw))
-        return o
+    sense = SenseHat()
+    o = sense.get_orientation()
+    a = sense.get_accelerometer_raw()
+    data = dict([("orientation", o), ("acceleration", a)])
+    print(data)
+    return data
 
 
 def send_things(data):
-    req = urllib3.Request("https://dogsplaypokemon.localtunnel.me")
-    req.add_header("Content-Type", "application/json")
-    try:
-        response = urllib3.urlopen(req, data)
-        print("Success!: " + response)
-    except NewConnectionError as ex:
-        print("Fail!: " + ex.message)
+    http = urllib3.PoolManager()
+    response = http.request("POST",
+                            "https://dogsplaypokemon.localtunnel.me",
+                            body=json.dumps(data).encode("utf-8"),
+                            headers={"Content-Type": "application/json"})
+    code = response.status
+    if code == 200 or code == 201:
+        print("Success!: " + code)
+    else:
+        print("Fail!: " + code)
 
 
 if __name__ == '__main__':
